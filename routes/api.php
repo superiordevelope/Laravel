1b84e22741386d8c364b5c50082af1c4a0b45413
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2020-07-28 17:23:19 +0300

    API

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/forecast', 'WeatherController@forecast');
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
