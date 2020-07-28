469e974b2dbd557fcd61a621f3394b50b9f8740d
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2020-07-28 14:44:25 +0300

    Alert component.

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="alert" :class="alertType">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+
+    props: {
+        type: {
+            type: String,
+            default: 'danger'
+        }
+    },
+
+    computed: {
+        alertType() {
+            return [`alert-${this.type}`];
+        }
+    }
+}
+</script>
